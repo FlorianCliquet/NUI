4f13e746780a6b79e240c77e2d6d370c65fca745
Oetgin <60893816+Oetgin@users.noreply.github.com>
2024-06-12 23:29:25 +0200

    Gateway and local ip

--- BACKEND/SourceCode/main.py
+++ BACKEND/SourceCode/main.py
@@ -4,23 +4,42 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_restful import Resource, Api
 from flask_caching import Cache
+from netifaces import gateways, ifaddresses, AF_INET
+from socket import gethostbyname, gethostname
 
 #-------------------------------------------------------------------------------------#
 
 
-#--------------------------------->[NMAP]<------------------------------------------#
+#--------------------------------->[NETWORK]<------------------------------------------#
 nm = nmap.PortScanner()
+
 def ping_scan():
     """
     This function is used to scan the network and get the list of active hosts.
     """
     nm.scan("192.168.1.0/24", arguments="-sn")
+
+
 def host_info(ip):
     """
     This function is used to get the full info about an active host.
     """
     nm.scan(ip, arguments="-O")
     return nm[ip]
+
+
+def get_gateway():
+    """
+    This function is used to get the default gateway.
+    """
+    return gateways()['default'][AF_INET][0]
+
+def get_ip():
+    """
+    This function is used to get the ip address of the device.
+    """
+    return gethostbyname(gethostname())
+
 def tests():
     """
     This function is used to test the function.
@@ -32,7 +51,7 @@ def tests():
         print(nm[host])
     print(host_info("192.168.1.27"))
 
-#-----------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------#
 
 
 #--------------------------------->[FLASK]<------------------------------------------#
@@ -55,6 +74,14 @@ class HostInfoAPI(Resource):
     def get(self, ip):
         return jsonify(host_info(ip))
 
+class GetGatewayAPI(Resource):
+    def get(self):
+        return get_gateway()
+
+class GetIPAPI(Resource):
+    def get(self):
+        return get_ip() 
+
 class ClearCacheAPI(Resource):
     def post(self):
         # Delete the cache
@@ -64,6 +91,8 @@ class ClearCacheAPI(Resource):
 
 api.add_resource(PingScanAPI, "/api/ping_scan")
 api.add_resource(HostInfoAPI, "/api/host_info/<string:ip>")
+api.add_resource(GetGatewayAPI, "/api/get_gateway")
+api.add_resource(GetIPAPI, "/api/get_ip")
 api.add_resource(ClearCacheAPI, "/api/clear_cache")
 
 

10652d43adfc2f37eaa2983603b8fa866c672642
FlorianCliquet <florian.cliquet.pro@gmail.com>
2024-06-12 19:25:45 +0200

    fix backend merge conflict

--- BACKEND/SourceCode/main.py
+++ BACKEND/SourceCode/main.py
@@ -7,22 +7,17 @@ from flask_restful import Resource, Api
 
 #--------------------------------->[NMAP]<------------------------------------------#
 nm = nmap.PortScanner()
-
 def ping_scan():
     """
     This function is used to scan the network and get the list of active hosts.
     """
     nm.scan("192.168.1.0/24", arguments="-sn")
-
-
 def host_info(ip):
     """
     This function is used to get the full info about an active host.
     """
     nm.scan(ip, arguments="-O")
     return nm[ip]
-
-
 def tests():
     """
     This function is used to test the function.
@@ -41,14 +36,21 @@ def tests():
 app = Flask(__name__)
 api = Api(app)
 
-@app.route("/api/ping_scan", methods=["GET"])
-def get_ping_scan():
-    """
-    This function is used to get the list of active hosts.
-    """
-    ping_scan()
-    hosts = nm.all_hosts()
-    return jsonify(hosts)
+class PingScanAPI(Resource):
+    def get(self):
+        ping_scan()
+        hosts = []
+        for host in nm.all_hosts():
+            hosts.append(nm[host])
+        return jsonify(hosts)
+
+class HostInfoAPI(Resource):
+    def get(self, ip):
+        return jsonify(host_info(ip))
+
+
+api.add_resource(PingScanAPI, "/api/ping_scan")
+api.add_resource(HostInfoAPI, "/api/host_info/<string:ip>")
 
 
 if __name__ == '__main__':
